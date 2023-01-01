26110c6f85d3c67f97d6fe4fc47a0493827ea327
iqra-shams <iqra.shams339@gmail.com>
2023-01-01 12:37:48 +0500

    "Done"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,61 +1,15 @@
 import './App.css';
-import React, { useState} from 'react';
+// import React, { useState} from 'react';
+//import JsonProblem from './COMPONENTS/S3Q3';
+import Pets from './COMPONENTS/S4Q1';
 
 function App() {
-  // const [loc, setLoc] = useState('Rawalpindi')
-  const [data, setData] = useState({});
-  const [isShown, setIsShown] = useState(false)
-  console.log(data);
-  const apiFetch = () =>
-  {
-    var loc1 = prompt('Enter location');
-    // var loc2 = setLoc(loc1);
-    if(loc1 === null)
-    {
-      loc1 = 'Rawalpindi';
-
-    }
-    fetch(`https://api.weatherapi.com/v1/current.json?key=39da29e9742d4a4f8db53832221012&q=${loc1}`)
-    .then(res => res.json())
-    .then(result => {
-      setData(result)
-      console.log(result)})
-      
-    }
-
-    const result = () =>
-    {
-      setIsShown(true)
-    }
   
+ return(
  
-  return (
-
-      <>
-    <div className="App">
-      {/* <input type="text" onChange={(e)=>setLoc(e.target.value) } value={loc} /> */}
-
-      <h1>Check Temperature</h1>
-
-      <button onClick={apiFetch}>Click Me to set location</button>
-      <br />
-      <br />
-
-      <button onClick={result}>Show Temperature</button>
-      {
-        isShown === true ? 
-        <>
-        <p>{data.location.name}</p>
-        <p id='para1'>{data.location.country}</p>
-        <img src={data.current.condition.icon} alt="Loading Error" />
-        <p>{data.current.condition.text}</p>
-        <p>{data.current.temp_c}  </p> 
-        </>
-        : null
-      }
-    </div>
-      </>
-  );
-}
-
+// <JsonProblem/>
+ <Pets/>
+ );
+ 
+ }
 export default App;
---
+++ my-app/src/COMPONENTS/S4Q1.js
@@ -0,0 +1,36 @@
+import React from "react";
+function Pets() {
+  const [isShown, setIsShown] = React.useState(0);
+  const [isVisible, setIsVisible] = React.useState(true);
+  const fetchAPI = () => {
+    if (isShown === 0) {
+      fetch("https://learnwebcode.github.io/json-example/animals-1.json")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+      setIsShown(1);
+    } else if (isShown === 1) {
+      fetch("https://learnwebcode.github.io/json-example/animals-2.json")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+      setIsShown(2);
+      setIsVisible(true);
+    }
+     else if (isShown === 2) {
+      fetchAPI("https://learnwebcode.github.io/json-example/animals-3.json")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+      setIsShown(0);
+      setIsVisible(false);
+    }
+  };
+  console.log(isShown);
+  console.log(isVisible);
+  return (
+    <>
+    <h1> PETS DATA</h1>
+      {isVisible === true ? <button onClick={fetchAPI}>Click me</button> : null}
+    </>
+  );
+}
+
+export default Pets;
